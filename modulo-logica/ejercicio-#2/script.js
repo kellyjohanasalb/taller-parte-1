80f3c4ca149bedefecbd62d6602e0bbb92475002
kelly saldaña <kellysalb@hotmail.com>
2023-11-14 11:59:35 -0500

    segundo-ejercicio

---
+++ modulo-logica/ejercicio-#2/script.js
@@ -0,0 +1,11 @@
+//function
+function suma(a, b) {
+    return a + b;
+}
+
+console.log('Suma:', suma(5, 3));
+
+//Arrow function
+const suma= (a, b) => a + b;
+
+console.log("Arrow-function:", suma(5, 3)); 
